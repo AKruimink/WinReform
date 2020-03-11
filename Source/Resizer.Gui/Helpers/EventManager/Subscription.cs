0c9a56ccff8d38d4993f1db488033a5c8fcd86f1
AKruimink <AlwinKruimink@gmail.com>
2020-03-11 20:36:51 +0000

    Add struct that defines a subscription to a event

---
+++ Source/Resizer.Gui/Helpers/EventManager/Subscription.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+namespace Resizer.Gui.Helpers.EventManager
+{
+    /// <summary>
+    /// Defines a event subscription
+    /// </summary>
+    internal struct Subscription
+    {
+        /// <summary>
+        /// The target instance to invoke the handler on
+        /// </summary>
+        public WeakReference? Subscriber { get; }
+
+        /// <summary>
+        /// The event handler
+        /// </summary>
+        public MethodInfo Handler { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="Subscription"/>
+        /// </summary>
+        /// <param name="subscriber">The <see cref="WeakReference"/> of the target instance to invoke on</param>
+        /// <param name="handler">The <see cref="MethodInfo"/> of the event handler</param>
+        public Subscription(in WeakReference? subscriber, in MethodInfo? handler)
+        {
+            Subscriber = subscriber;
+            Handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+    }
+}
