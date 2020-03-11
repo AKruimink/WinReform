ec491995e420e5d14fadf0d05a8f9212a457e4a5
AKruimink <AlwinKruimink@gmail.com>
2020-03-11 20:38:31 +0000

    Add custom exception to handle invalid parameter counts

---
+++ Source/Resizer.Gui/Helpers/EventManager/InvalidHandleEventException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace Resizer.Gui.Helpers.EventManager
+{
+    /// <summary>
+    /// Defines a invalid handle error that occures during <see cref="WeakEventManager.HandleEvent"/> execution
+    /// </summary>
+    public class InvalidHandleEventException : Exception
+    {
+        /// <summary>
+        /// Intializes a new instance of <see cref="InvalidHandleEventException"/>
+        /// </summary>
+        /// <param name="message">The message</param>
+        /// <param name="targetParameterCountException">The <see cref="TargetParameterCountException"/> thrown</param>
+        public InvalidHandleEventException(string message, TargetParameterCountException targetParameterCountException)
+            : base(message, targetParameterCountException)
+        {
+        }
+    }
+}
