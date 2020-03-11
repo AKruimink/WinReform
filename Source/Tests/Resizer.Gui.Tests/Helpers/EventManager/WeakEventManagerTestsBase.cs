cdb785f7e36ee5c0bf6e2cf4104243bd0584c2a4
AKruimink <AlwinKruimink@gmail.com>
2020-03-11 20:47:11 +0000

    Add weak event manager test base

---
+++ Source/Tests/Resizer.Gui.Tests/Helpers/EventManager/WeakEventManagerTestsBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Resizer.Gui.Helpers.EventManager;
+
+namespace Resizer.Gui.Tests.Helpers.EventManager
+{
+    /// <summary>
+    /// Defines a base class for the Weak Event Manager tests that provides methods, propeties and variables
+    /// </summary>
+    public abstract class WeakEventManagerTestsBase
+    {
+        public readonly IWeakEventManager WeakEventManager = new WeakEventManager();
+
+        public readonly IWeakEventManager<string> WeakEventManagerType = new WeakEventManager<string>();
+
+        public event EventHandler TestEvent
+        {
+            add => WeakEventManager.AddEventHandler(value);
+            remove => WeakEventManager.RemoveEventHandler(value);
+        }
+    }
+}
