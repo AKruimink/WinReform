cf563f2113c861e4ed876376c3e51e6bc2d00eee
AKruimink <AlwinKruimink@gmail.com>
2020-05-23 03:44:38 +0100

    Add DelegateReference

---
+++ Source/Resizer.Gui/Helpers/Messenger/IDelegateReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Resizer.Gui.Helpers.Messenger
+{
+    /// <summary>
+    /// Represents a class that holds a reference to a delegate
+    /// </summary>
+    public interface IDelegateReference
+    {
+        /// <summary>
+        /// Gets the referenced <see cref="Delegate"/> object, Returns <see cref="null"/> if non exist
+        /// </summary>
+        Delegate? GetDelegate { get; }
+    }
+}
