cbe325e357d5680ca63139a12d88d2b3ed3c70fd
AKruimink <AlwinKruimink@gmail.com>
2020-05-24 03:34:02 +0100

    Add thread options for event publishing

---
+++ Source/Resizer.Gui/Helpers/Messenger/ThreadOption.cs
@@ -0,0 +1,23 @@
+﻿namespace Resizer.Gui.Helpers.Messenger
+{
+    /// <summary>
+    /// Represents the thread on which a <see cref="PubSubEvent"/> subscriber will be called
+    /// </summary>
+    public enum ThreadOption
+    {
+        /// <summary>
+        /// The subscriber will be called on the same thread as the <see cref="PubSubEvent"/> was published
+        /// </summary>
+        PublisherThread,
+
+        /// <summary>
+        /// The subscriber will be called on the UI thread
+        /// </summary>
+        UIThread,
+
+        /// <summary>
+        /// The subscriber will be called on a asynchronous background thread
+        /// </summary>
+        BackgroundThread
+    }
+}
