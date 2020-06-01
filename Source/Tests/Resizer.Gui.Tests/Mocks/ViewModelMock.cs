eb431dc9d7ac8ec2e017da37778657163955199f
AKruimink <AlwinKruimink@gmail.com>
2020-06-01 21:11:54 +0100

    Add a mock for a basic view model

---
+++ Source/Tests/Resizer.Gui.Tests/Mocks/ViewModelMock.cs
@@ -0,0 +1,32 @@
+﻿using Resizer.Gui.Common.ViewModel;
+
+namespace Resizer.Gui.Tests.Mocks
+{
+    /// <summary>
+    /// Defines a mock implementation of a view model 
+    /// </summary>
+    public class ViewModelMock : ViewModelBase
+    {
+        /// <summary>
+        /// Random integer used for testing
+        /// </summary>
+        private int _intProperty;
+
+        /// <summary>
+        /// Gets or Sets the int property and triggers the <see cref="ViewModelBase"/> set property
+        /// </summary>
+        public int IntProperty
+        {
+            get => _intProperty;
+            set => SetProperty(ref _intProperty, value);
+        }
+
+        /// <summary>
+        /// Raises the <see cref="IntProperty"/>
+        /// </summary>
+        public void InvokeOnPropertyChanged()
+        {
+            RaisePropertyChanged(nameof(IntProperty));
+        }
+    }
+}
