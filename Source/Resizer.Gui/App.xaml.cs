7127c9185633840e9eee98f5c8828c1e075e6b0c
AKruimink <AlwinKruimink@gmail.com>
2020-06-13 03:22:49 +0100

    Updated App to allow for OnStatup and OnExit execution

--- Source/Resizer.Gui/App.xaml.cs
+++ Source/Resizer.Gui/App.xaml.cs
@@ -7,5 +7,25 @@ namespace Resizer.Gui
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Creates all the required objects on startup
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new MainWindow();
+            window.Show();
+        }
+
+        /// <summary>
+        /// Disposes of all unmanaged objects on exit
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
     }
 }
