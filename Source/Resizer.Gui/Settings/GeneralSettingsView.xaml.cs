89e36678367b904697907fa7e56c34578bba6e3d
AKruimink <AlwinKruimink@gmail.com>
2020-06-15 00:27:13 +0100

    Add simple layout for the general settings view

---
+++ Source/Resizer.Gui/Settings/GeneralSettingsView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resizer.Gui.Settings
+{
+    /// <summary>
+    /// Interaction logic for GeneralSettingsView.xaml
+    /// </summary>
+    public partial class GeneralSettingsView : UserControl
+    {
+        /// <summary>
+        /// Create a new instance of <see cref="GeneralSettingsView"/>
+        /// </summary>
+        public GeneralSettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
