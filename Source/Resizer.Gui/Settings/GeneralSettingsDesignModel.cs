83c9ae8d8127239aaac5788e89076aacf921f934
AKruimink <AlwinKruimink@gmail.com>
2020-06-15 02:27:27 +0100

    Add general settings design model

---
+++ Source/Resizer.Gui/Settings/GeneralSettingsDesignModel.cs
@@ -0,0 +1,33 @@
+﻿namespace Resizer.Gui.Settings
+{
+    /// <summary>
+    /// Defines a class that provides design time general application settings data
+    /// </summary>
+    public class GeneralSettingsDesignModel
+    {
+        /// <summary>
+        /// Gets or Sets an idicator that defines if dark theme should be used
+        /// </summary>
+        public bool UseDarkTheme { get; set; }
+
+        /// <summary>
+        /// Gets or Sets an idicator that defines if the window should be minimized when closed
+        /// </summary>
+        public bool MinimizeOnClose { get; set; }
+
+        /// <summary>
+        /// Gets or Sets an idicator that defines if the window should be minimized to the system tray
+        /// </summary>
+        public bool MinimizeToSystemTray { get; set; }
+
+        /// <summary>
+        /// Create a new instance of the <see cref="GeneralSettingsDesignModel"/>
+        /// </summary>
+        public GeneralSettingsDesignModel()
+        {
+            UseDarkTheme = true;
+            MinimizeOnClose = false;
+            MinimizeToSystemTray = true;
+        }
+    }
+}
