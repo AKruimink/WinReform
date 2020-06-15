2fc5f985abdbde7a7c352a5f7b81d45272eb611e
AKruimink <AlwinKruimink@gmail.com>
2020-06-15 03:49:52 +0100

    Add a settings store to save and load setting files

---
+++ Source/Resizer.Domain/Settings/ISettingsStore.cs
@@ -0,0 +1,23 @@
+﻿namespace Resizer.Domain.Settings
+{
+    /// <summary>
+    /// Represents a class that loads and saves settings
+    /// </summary>
+    public interface ISettingsStore
+    {
+        /// <summary>
+        /// Load settings
+        /// <remarks>A new file with default settings will be created if non exists</remarks>
+        /// </summary>
+        /// <typeparam name="TSettings">The type of the settings to load</typeparam>
+        /// <returns>Returns the settings of type <see cref="TSettings"/></returns>
+        TSettings Load<TSettings>() where TSettings : new();
+
+        /// <summary>
+        /// Saves settings
+        /// </summary>
+        /// <typeparam name="TSettings">The type of the settings to save</typeparam>
+        /// <param name="settings">The settings to be saved</param>
+        void Save<TSettings>(TSettings settings);
+    }
+}
---
+++ Source/Resizer.Domain/Settings/SettingsStore.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Resizer.Domain.Settings
+{
+    /// <summary>
+    /// Defines a class that loads and saves settings
+    /// </summary>
+    public class SettingsStore : ISettingsStore
+    {
+        /// <summary>
+        /// Path to where the setting files are saved
+        /// </summary>
+        private readonly string _filePath;
+
+        /// <summary>
+        /// Create a new instance of the <see cref="SettingsStore"/>
+        /// </summary>
+        public SettingsStore()
+        {
+            _filePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        ///<inheritdoc/>
+        public TSettings Load<TSettings>() where TSettings : new()
+        {
+            var file = Path.Combine(_filePath, $"{typeof(TSettings).Name}.xml");
+            var fileInfo = new FileInfo(file);
+
+            // Create default settings if non exist
+            if(!fileInfo.Exists || fileInfo.Length == 0)
+            {
+                Save(new TSettings());
+            }
+
+            var serializer = new XmlSerializer(typeof(TSettings));
+
+            using var reader = new StreamReader(file);
+            return (TSettings)serializer.Deserialize(reader);
+        }
+
+        ///<inheritdoc/>
+        public void Save<TSettings>(TSettings settings)
+        {
+            var file = Path.Combine(_filePath, $"{typeof(TSettings).Name}.xml");
+            var settingsFile = new XmlDocument();
+            var serializer = new XmlSerializer(typeof(TSettings));
+
+            using var stream = new MemoryStream();
+            serializer.Serialize(stream, settings);
+            stream.Position = 0;
+            settingsFile.Load(stream);
+            settingsFile.Save(file);
+            stream.Close();
+        }
+    }
+}
