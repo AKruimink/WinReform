c4e3b72020945d956a33fad25808f2d1554fa687
AKruimink <AlwinKruimink@gmail.com>
2020-06-15 14:59:06 +0100

    Add event to handle setting changes

---
+++ Source/Resizer.Domain/Infrastructure/Events/GeneralSettingsChangedEvent.cs
@@ -0,0 +1,11 @@
+﻿using Resizer.Gui.Infrastructure.Common.Messenger;
+
+namespace Resizer.Domain.Infrastructure.Events
+{
+    /// <summary>
+    /// Defines a class that acts as a contract for when the general settings have changed
+    /// </summary>
+    public class GeneralSettingsChangedEvent : PubSubEvent
+    {
+    }
+}
