d7212a9cf1136ab2250ef208f8918de5737c40bf
AKruimink <AlwinKruimink@gmail.com>
2020-06-15 11:48:11 +0100

    Add a settings class that houses a given setting

---
+++ Source/Resizer.Domain/Settings/ISettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Resizer.Domain.Settings
+{
+    /// <summary>
+    /// Represents a class that holds settings
+    /// </summary>
+    /// <typeparam name="TSetting">The settings type</typeparam>
+    public interface ISettings<TSetting>
+    {
+        /// <summary>
+        /// Save settings
+        /// </summary>
+        /// <param name="item">The <see cref="Settings"/> to save</param>
+        void Save(TSetting settings);
+    }
+}
