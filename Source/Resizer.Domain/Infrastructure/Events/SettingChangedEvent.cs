8b645e1da087b341f96a8795d256ec5daf9f3daf
AKruimink <AlwinKruimink@gmail.com>
2020-06-16 02:07:32 +0100

    Generalized GeneralSettingsChangedEvent and send generic type with it

---
+++ Source/Resizer.Domain/Infrastructure/Events/SettingChangedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Resizer.Domain.Infrastructure.Messenger;
+
+namespace Resizer.Domain.Infrastructure.Events
+{
+    /// <summary>
+    /// Defines a class that acts as a contract for when a settings have changed
+    /// </summary>
+    public class SettingChangedEvent : PubSubEvent<Type>
+    {
+    }
+}
