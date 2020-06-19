637d6eed38e567fc4d74c2bca215c6da4e82a5df
AKruimink <AlwinKruimink@gmail.com>
2020-06-19 20:15:44 +0100

    Updated ApplicationSettingsViewModel to save when ever a change is made

--- Source/Resizer.Gui/Settings/ApplicationSettingsViewModel.cs
+++ Source/Resizer.Gui/Settings/ApplicationSettingsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Resizer.Gui.Infrastructure.Common.ViewModel;
+﻿using Resizer.Domain.Settings;
+using Resizer.Gui.Infrastructure.Common.ViewModel;
 
 namespace Resizer.Gui.Settings
 {
@@ -13,10 +14,15 @@ namespace Resizer.Gui.Settings
         public bool UseDarkTheme
         {
             get => _useDarkTheme;
-            set => SetProperty(ref _useDarkTheme, value);
+            set
+            {
+                SetProperty(ref _useDarkTheme, value);
+                _settings.CurrentSetting.UseDarkTheme = value;
+                SaveSettings();
+            }
         }
 
-        private bool _useDarkTheme;
+        private bool _useDarkTheme = true;
 
         /// <summary>
         /// Gets or Sets an idicator that defines if the window should be minimized when closed
@@ -24,7 +30,12 @@ namespace Resizer.Gui.Settings
         public bool MinimizeOnClose
         {
             get => _minimizeOnClose;
-            set => SetProperty(ref _minimizeOnClose, value);
+            set
+            {
+                SetProperty(ref _minimizeOnClose, value);
+                _settings.CurrentSetting.MinimizeOnClose = value;
+                SaveSettings();
+            }
         }
 
         private bool _minimizeOnClose;
@@ -35,17 +46,39 @@ namespace Resizer.Gui.Settings
         public bool MinimizeToSystemTray
         {
             get => _minimizeToSystemTray;
-            set => SetProperty(ref _minimizeToSystemTray, value);
+            set
+            {
+                SetProperty(ref _minimizeToSystemTray, value);
+                _settings.CurrentSetting.MinimizeToSystemTray = value;
+                SaveSettings();
+            }
         }
 
         private bool _minimizeToSystemTray;
 
         /// <summary>
+        /// <see cref="ISetting{TSetting}"/> containing the <see cref="ApplicationSettings"/>
+        /// </summary>
+        private readonly ISetting<ApplicationSettings> _settings;
+
+        /// <summary>
         /// Create a new instance of the <see cref="ApplicationSettingsViewModel"/>
         /// </summary>
-        public ApplicationSettingsViewModel()
+        public ApplicationSettingsViewModel(ISettingFactory settingfactory)
         {
+            _settings = settingfactory.Create<ApplicationSettings>();
 
+            UseDarkTheme = _settings.CurrentSetting.UseDarkTheme;
+            MinimizeOnClose = _settings.CurrentSetting.MinimizeOnClose;
+            MinimizeToSystemTray = _settings.CurrentSetting.MinimizeToSystemTray;
+        }
+
+        /// <summary>
+        /// Save the settings
+        /// </summary>
+        private void SaveSettings()
+        {
+            _settings.Save();
         }
     }
 }
