e7a0da0f1ebace60ff12b19713547e7c7a4c3382
AKruimink <AlwinKruimink@gmail.com>
2020-07-08 03:29:36 +0100

    Add a viewmodel and view for the active windows

---
+++ Source/Resizer.Gui/Sizer/ActiveWindowsView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Resizer.Gui.Sizer
+{
+    /// <summary>
+    /// Interaction logic for ActiveWindowsView.xaml
+    /// </summary>
+    public partial class ActiveWindowsView : UserControl
+    {
+        /// <summary>
+        /// Create a new instance of <see cref="ActiveWindowsView"/>
+        /// </summary>
+        public ActiveWindowsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Source/Resizer.Gui/Sizer/SizerViewModel.cs
@@ -0,0 +1,9 @@
+namespace Resizer.Gui.Sizer
+{
+    /// <summary>
+    /// Defines a class that handles the active windows and managing of them
+    /// </summary>
+    public class SizerViewModel : ViewModelBase
+    {
+    }
+}
