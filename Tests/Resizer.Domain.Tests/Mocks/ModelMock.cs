6c41f04c212e2aa209e05362ed444f32b2cf204e
AKruimink <AlwinKruimink@gmail.com>
2020-07-23 15:04:21 +0100

    Add tests for the ObservableCollection Extension

--- Tests/Resizer.Domain.Tests/Mocks/ModelMock.cs
+++ Tests/Resizer.Domain.Tests/Mocks/ModelMock.cs
@@ -18,14 +18,19 @@ namespace Resizer.Domain.Tests.Mocks
         /// <summary>
         /// Gets or Sets a random <see cref="string"/> property
         /// </summary>
-        public int SomeText { get; set; }
+        public string SomeText { get; set; }
+
+        /// <summary>
+        /// Gets or Sets a random <see cref="int"/> property
+        /// </summary>
+        public int SomeNumber { get; set; }
 
         /// <summary>
         /// Comapares the current <see cref="ModelMock"/> to a given <see cref="ModelMock"/>
         /// </summary>
         /// <param name="other"><see cref="ModelMock"/> to compare to the current instance</param>
         /// <returns>Returns <see langword="true"/> if the current istance is equal to the given <see cref="ModelMock"/>, otherwise returns <see langword="false"/></returns>
-        public bool Equals([AllowNull] ModelMock other) => other?.Id == Id && other?.SomeText == SomeText;
+        public bool Equals([AllowNull] ModelMock other) => other?.Id == Id && other?.SomeText == SomeText && other?.SomeNumber == SomeNumber;
 
         /// <summary>
         /// Comapares the current <see cref="ModelMock"/> to a given <see cref="object"/>
@@ -38,7 +43,7 @@ namespace Resizer.Domain.Tests.Mocks
         /// Gets the hashCode of the <see cref="ModelMock"/>
         /// </summary>
         /// <returns>Returns <see cref="int"/> containing a unique hashcode that represents the instance of the current <see cref="ModelMock"/></returns>
-        public override int GetHashCode() => (Id, SomeText).GetHashCode();
+        public override int GetHashCode() => (Id, SomeText, SomeNumber).GetHashCode();
 
         /// <summary>
         /// Compare if the current <see cref="MockModel"/> represents the same item as a given <see cref="MockModel"/>
