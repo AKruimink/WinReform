c627a9b0c226a78ea81c9877bfc9b838211a9481
AKruimink <AlwinKruimink@gmail.com>
2020-07-24 22:08:28 +0100

    Add tests and mock implementation for the WindowService

---
+++ Tests/Resizer.Domain.Tests/Windows/WindowServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resizer.Domain.Tests.Windows
+{
+    /// <summary>
+    /// Tests for the <see cref="WindowService"/>
+    /// </summary>
+    public class WindowServiceTests
+    {
+    }
+}
