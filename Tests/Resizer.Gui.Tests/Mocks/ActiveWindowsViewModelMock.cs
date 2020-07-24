19b2448df36daae38718acf509c9755735bb5bdb
AKruimink <AlwinKruimink@gmail.com>
2020-07-24 01:02:09 +0100

    Add mocks for the viewmodels

---
+++ Tests/Resizer.Gui.Tests/Mocks/ActiveWindowsViewModelMock.cs
@@ -0,0 +1,20 @@
+﻿using Resizer.Gui.ActiveWindows;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Resizer.Gui.Tests.Mocks
+{
+    /// <summary>
+    /// Mock implementation of <see cref="IActiveWindowsViewModel"/>
+    /// </summary>
+    public class ActiveWindowsViewModelMock : IActiveWindowsViewModel
+    {
+        ///<inheritdoc/>
+        public ObservableCollection<Domain.Windows.Window> ActiveWindows { get; set; } = new ObservableCollection<Domain.Windows.Window>();
+
+        ///<inheritdoc/>
+        public ObservableCollection<Domain.Windows.Window> SelectedActiveWindows { get; set; } = new ObservableCollection<Domain.Windows.Window>();
+    }
+}
---
+++ Tests/Resizer.Gui.Tests/Mocks/ApplicationSettingsViewModelMock.cs
@@ -0,0 +1,19 @@
+﻿using Resizer.Gui.Settings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resizer.Gui.Tests.Mocks
+{
+    /// <summary>
+    /// Mock implementation of <see cref="IApplicationSettingsViewModel"/>
+    /// </summary>
+    public class ApplicationSettingsViewModelMock : IApplicationSettingsViewModel
+    {
+        ///<inheritdoc/>
+        public bool UseDarkTheme { get; set; }
+
+        ///<inheritdoc/>
+        public bool MinimizeOnClose { get; set; }
+    }
+}
