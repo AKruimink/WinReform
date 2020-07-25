7370c41660b0109e6f752a7520798777ec93bfa0
AKruimink <AlwinKruimink@gmail.com>
2020-07-25 02:34:36 +0100

    Add tests for the string extensions

---
+++ Tests/Resizer.Gui.Tests/Infrastructure/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Xunit;
+
+namespace Resizer.Gui.Tests.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Tests for the <see cref="StringExtensions"/>
+    /// </summary>
+    public class StringExtensionsTests
+    {
+        #region UpdateCollection Tests
+
+        [Fact]
+        public void Contains_DoesContainString_ShouldReturnTrue()
+        {
+            // Prepare
+            var source = "Some Random String";
+            var other = "Random";
+
+            // Act
+            bool result = source.Contains(other, StringComparison.OrdinalIgnoreCase);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Contains_DoesNotContainString_ShouldReturnFalse()
+        {
+            // Prepare
+            var source = "Some Random String";
+            var other = "Invalid";
+
+            // Act
+            bool result = source.Contains(other, StringComparison.OrdinalIgnoreCase);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Contains_CaseSensative_ShouldReturnFalse()
+        {
+            // Prepare
+            var source = "Some Random String";
+            var other = "random";
+
+            // Act
+            bool result = source.Contains(other, StringComparison.InvariantCulture);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        #endregion
+    }
+}
