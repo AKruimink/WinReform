3ed80f137578803110059af3dfdeb35d0901d8dc
AKruimink <AlwinKruimink@gmail.com>
2020-08-02 22:53:43 +0100

    Fixed missing interface inplementation

--- src/WinReform/WinReform.Domain/Process/ProcessService.cs
+++ src/WinReform/WinReform.Domain/Process/ProcessService.cs
@@ -8,7 +8,7 @@ namespace WinReform.Domain.Process
     /// <summary>
     /// Defines a class that acts as a service for managing active processes
     /// </summary>
-    public class ProcessService
+    public class ProcessService : IProcessService
     {
         /// <inheritdoc/>
         public List<System.Diagnostics.Process> GetActiveProcesses() => System.Diagnostics.Process.GetProcesses().ToList();
