d27dcd95ad8081cd90ac5415d5a41f19e8e41bd7
AKruimink <AlwinKruimink@gmail.com>
2020-08-13 03:43:44 +0100

    Add test fixtures for testing of abstractions and usage of models and classes that can't be mocked

---
+++ src/Tests/WinReform.Tests.Fixtures/ClassFixture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinReform.Tests.Fixtures
+{
+    /// <summary>
+    /// Defines a class that represents a class fixture that provides a fake class for testing puprose
+    /// </summary>
+    public class ClassFixture
+    {
+    }
+}
