07d5feb4eafc927310ead1ca886bd8d52bb16e55
AKruimink <AlwinKruimink@gmail.com>
2020-08-20 02:38:37 +0100

    Add process and window process fixtures to test with process data

---
+++ src/Tests/WinReform.Tests.Process.Fixture/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WinReform.Tests.Process.Fixture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(); // Keeps process active
+        }
+    }
+}
