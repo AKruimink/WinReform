5a513b6eb4820767f390d2c97b83083420e3b971
AKruimink <AlwinKruimink@gmail.com>
2020-08-23 04:31:45 +0100

    Add SetWindowPos to the WinApiService

--- src/WinReform/WinReform.Domain/WinApi/IWinApiService.cs
+++ src/WinReform/WinReform.Domain/WinApi/IWinApiService.cs
@@ -21,6 +21,14 @@ namespace WinReform.Domain.WinApi
         Rect GetWindowRect(IntPtr hwnd);
 
         /// <summary>
+        /// Sets a new position for a given window
+        /// </summary>
+        /// <param name="hwnd"><see cref="IntPtr"/> containing the handle of the window to set the position of</param>
+        /// <param name="position"><see cref="Rect"/> containing the new position of the window</param>
+        /// <param name="uFlags"><see cref="SwpType"/> containing the sizing and position flags to be used</param>
+        void SetWindowPos(IntPtr hwnd, Rect position, SwpType uFlags);
+
+        /// <summary>
         /// Gets values of a window of a specified <see cref="GwlType"/>
         /// </summary>
         /// <param name="hwnd"><see cref="IntPtr"/> containing the handle of the window to retrieve information from</param>
@@ -32,7 +40,7 @@ namespace WinReform.Domain.WinApi
         /// Sets new values of a window for a specified <see cref="GwlType"/>
         /// </summary>
         /// <param name="hwnd"><see cref="IntPtr"/> containing the handle of the window to retrieve information from</param>
-        /// <param name="nIndex"><see cref="GwlType"/> of the data to be requested</param>
+        /// <param name="nIndex"><see cref="GwlType"/> of the data to be set</param>
         /// <param name="dwNewLong"><see cref="IntPtr"/> containing the new values to be set</param>
         /// <returns>Returns <see cref="IntPtr"/> with the newly set values</returns>
         IntPtr SetWindowLongPtr(IntPtr hwnd, GwlType nIndex, IntPtr dwNewLong);
