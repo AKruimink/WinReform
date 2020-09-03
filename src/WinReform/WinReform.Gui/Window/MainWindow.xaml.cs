bba7af80d01537285b3e9bcca50d288573d160ee
AKruimink <AlwinKruimink@gmail.com>
2020-09-03 01:54:11 +0100

    add scale value to the MainWindow that dictates the scale applied to the window

--- src/WinReform/WinReform.Gui/Window/MainWindow.xaml.cs
+++ src/WinReform/WinReform.Gui/Window/MainWindow.xaml.cs
@@ -11,6 +11,65 @@ namespace WinReform.Gui
     public partial class MainWindow : MetroWindow
     {
         /// <summary>
+        /// Gets or Sets the scale value used to scale transform elements based on current application resolution
+        /// </summary>
+        public static readonly DependencyProperty ScaleValueProperty = 
+            DependencyProperty.Register(
+                "ScaleValue", 
+                typeof(double), 
+                typeof(MainWindow), 
+                new UIPropertyMetadata(1.0, new PropertyChangedCallback(OnScaleValueChanged), new CoerceValueCallback(OnCoerceScaleValue)));
+
+        /// <summary>
+        /// Invoked every time <see cref="ScaleValueProperty"/> changed it value
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="e"></param>
+        private static void OnScaleValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Invoked each time <see cref="ScaleValueProperty"/> changed regardless if the value is different then the previouse and validates the new value
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="value"></param>
+        /// <returns>Returns <see cref="double"/> with the validated scale value</returns>
+        private static object OnCoerceScaleValue(DependencyObject o, object value)
+        {
+            var newValue = 1.0d;
+
+            if (double.IsNaN((double)value))
+            {
+                return newValue;
+            }
+
+            newValue = Math.Min(MaximumScaleValue, (double)value);
+            newValue = Math.Max(MinimumScaleValue, newValue);
+            
+            return newValue;
+        }
+
+        /// <summary>
+        /// Gets the maximum scale value allowed
+        /// </summary>
+        public static  double MaximumScaleValue { get; } = 1.0d;
+
+        /// <summary>
+        /// Gets the minimum scale value allowed
+        /// </summary>
+        public static double MinimumScaleValue { get; } = 0.7d;
+
+        /// <summary>
+        /// Gets or Sets the scale value used to scale transform the application
+        /// </summary>
+        public double ScaleValue
+        {
+            get => (double)GetValue(ScaleValueProperty);
+            set => SetValue(ScaleValueProperty, value);
+        }
+
+        /// <summary>
         /// Create a new instance of <see cref="MainWindow"/>
         /// </summary>
         public MainWindow()
